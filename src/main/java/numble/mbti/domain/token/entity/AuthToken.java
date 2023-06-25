c37ca0971b904c28bdbc0a6962ce4dfa5c05bd18
llsrrll96 <llsrrll96@gmail.com>
2023-06-25 22:12:49 +0900

    feat: jwt 토큰 기능

--- src/main/java/numble/mbti/domain/token/entity/AuthToken.java
+++ src/main/java/numble/mbti/domain/token/entity/AuthToken.java
@@ -2,18 +2,18 @@ package numble.mbti.domain.token.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import numble.mbti.domain.user.entity.User;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class AuthToken {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,8 +22,15 @@ public class AuthToken {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
-
     private String accessToken;
     private String refreshToken;
     private LocalDateTime refreshExpiredAt;
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
 }
