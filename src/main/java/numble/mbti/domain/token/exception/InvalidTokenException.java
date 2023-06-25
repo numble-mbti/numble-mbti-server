b46adf7bf99b4a451f9fc151283841db2930c652
llsrrll96 <llsrrll96@gmail.com>
2023-06-25 22:10:57 +0900

    feat: 예외 추가

---
+++ src/main/java/numble/mbti/domain/token/exception/InvalidTokenException.java
@@ -0,0 +1,12 @@
+package numble.mbti.domain.token.exception;
+
+public class InvalidTokenException extends BadRequestException {
+
+    private static final String ERROR_CODE = "INVALID_TOKEN";
+    private static final String SERVER_MESSAGE = "유효하지 않은 토큰으로 요청";
+    private static final String CLIENT_MESSAGE = "유효하지 않은 토큰입니다.";
+
+    public InvalidTokenException(String token) {
+        super(String.format("%s -> token: %s", SERVER_MESSAGE, token), CLIENT_MESSAGE, ERROR_CODE);
+    }
+}
