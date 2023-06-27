67d4885b9044522fb7372e22ca6f4c236ed1ac39
llsrrll96 <llsrrll96@gmail.com>
2023-06-28 01:00:50 +0900

    chore: Cors config

--- src/main/java/numble/mbti/domain/social/controller/SocialController.java
+++ src/main/java/numble/mbti/domain/social/controller/SocialController.java
@@ -13,13 +13,11 @@ import numble.mbti.domain.token.service.TokenService;
 import numble.mbti.domain.user.entity.User;
 import numble.mbti.domain.user.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
+@CrossOrigin("*")
 @RestController
 @RequiredArgsConstructor
 @Slf4j
--- src/main/java/numble/mbti/global/config/Config.java
+++ src/main/java/numble/mbti/global/config/Config.java
@@ -1,18 +1,22 @@
 package numble.mbti.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class Config implements WebMvcConfigurer {
-    public static final String ALLOWED_METHOD_NAMES = "GET,POST";
+    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
 
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedMethods(ALLOWED_METHOD_NAMES.split(","));
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+                .exposedHeaders(HttpHeaders.LOCATION);
         registry.addMapping("/oauth2/**")
-                .allowedMethods(ALLOWED_METHOD_NAMES.split(","));
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+                .exposedHeaders(HttpHeaders.LOCATION);
+
     }
 
 }
