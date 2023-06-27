cd90ddf4dcd5b69f48cc7e7e42627820c52da17b
llsrrll96 <llsrrll96@gmail.com>
2023-06-28 00:53:23 +0900

    chore: Cors config

--- src/main/java/numble/mbti/global/config/Config.java
+++ src/main/java/numble/mbti/global/config/Config.java
@@ -1,18 +1,18 @@
 package numble.mbti.global.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class Config implements WebMvcConfigurer {
-    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+    public static final String ALLOWED_METHOD_NAMES = "GET,POST";
 
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
-                .exposedHeaders(HttpHeaders.LOCATION);
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","));
+        registry.addMapping("/oauth2/**")
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","));
     }
 
 }
