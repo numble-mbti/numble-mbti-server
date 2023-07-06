8922bcc543d4bc27c3931fe50a6ae95a1c9e99d7
Kwonyeeun <80144039+Yeni924@users.noreply.github.com>
2023-07-06 20:19:04 +0900

    Hotfix/kye/유저 기록 저장 api 수정 (#25)
    * refac : record -> archive로 서비스 로직 이동
    * 예외 처리 추가
    * features 테이블 필요 컬럼 추가로 subContents 추가
    * 공통 예외 처리 추가 예정
    
    * fix : 유저 기록 저장 안되는 오류 수정
    * record -> archive로 수정 하면서 categoryId를 안넘겨줘서 발생함
    * record 관련 클래스 삭제

--- src/main/java/numble/api/record/controller/RecordController.java
+++ src/main/java/numble/api/record/controller/RecordController.java
@@ -5,8 +5,8 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import numble.api.record.controller.response.UserMbtiResultRequest;
-import numble.api.record.service.RecordService;
-import numble.mbti.domain.token.jwt.AuthorizationExtractor;
+import numble.api.record.service.ArchiveApiService;
+import numble.mbti.domain.archive.service.ArchiveService;
 import numble.mbti.domain.token.jwt.JwtTokenProvider;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,7 +22,7 @@ import jakarta.validation.Valid;
 @RequiredArgsConstructor
 public class RecordController {
 
-    private final RecordService recordService;
+    private final ArchiveApiService archiveApiService;
     private final JwtTokenProvider jwtTokenProvider;
 
     @Operation(summary = "유저 mbti 결과 저장", security = {@SecurityRequirement(name = "bearer-key")})
@@ -31,13 +31,14 @@ public class RecordController {
 
         String token = (String) request.getAttribute("token");
         Long userId = jwtTokenProvider.getUserIdFromToken(token);
+
         //Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         //String token = (String) authentication.getCredentials();
         // String token = AuthorizationExtractor.extract(request);
         //Long userId = jwtTokenProvider.getUserIdFromToken(token);
 
         if (userId != null) {
-            recordService.saveUserResult(userId, resultRequest);
+            archiveApiService.saveUserResult(userId, resultRequest);
             return ResponseEntity.ok().build();
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
--- src/main/java/numble/mbti/domain/record/repository/RecordRepository.java
+++
@@ -1,9 +0,0 @@
-package numble.mbti.domain.record.repository;
-
-import numble.mbti.domain.record.entity.UserRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RecordRepository extends JpaRepository<UserRecord, Long> {
-}
